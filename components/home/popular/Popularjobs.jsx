f3a3edae12b5238e4499c42632529af2a87250ce
NikPrivate <nikizzat0309@gmail.com>
2023-11-27 01:05:30 +0800

    'forth-commit'

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -7,11 +7,23 @@ import { useRouter } from 'expo-router'
 import styles from './popularjobs.style'
 import {COLORS, SIZES } from '../../../constants';
 import PopularJobCard from '../../common/cards/popular/PopularJobCard';
+import useFetch from '../../../hook/useFetch';
+
 
 const Popularjobs = () => {
   const router = useRouter();
-  const isLoading = false;
-  const error = false;
+  
+  const { data, isLoading, error } = useFetch
+  ('search', {
+    query: 'Reach developer',
+    num_pages: 1
+  });
+
+  const [selectedJob, setSelectedJob] = useState()
+
+  const handleCardPress = (item) => {
+
+  }
 
   return (
     <View style={styles.container}>
@@ -29,10 +41,12 @@ const Popularjobs = () => {
           <Text>Something went wrong</Text>
         ): (
           <FlatList
-            data={[1, 2, 3, 4]}
+            data={data}
             renderItem={({ item }) => (
               <PopularJobCard
                 item={item}
+                selectedJob={selectedJob}
+                handleCardPress={handleCardPress}
               />
             )}
             keyExtractor={item => item?.job_id}
